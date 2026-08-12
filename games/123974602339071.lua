439ee0bcea061e2754cb72e6c678d8cd151f9355
moon <277928006+uw2y@users.noreply.github.com>
2026-08-11 23:48:43 -0400

    Update 123974602339071.lua

--- games/123974602339071.lua
+++ games/123974602339071.lua
@@ -1,6 +1,22 @@
 local vape = shared.vape
 vape.run = function(x) return x() end;
 
+for _, v in {
+    'AntiFall', 'Fly', 'HighJump', 'HitBoxes', 'Invisible', 'Jesus', 'Killaura', 'LongJump',
+    'MouseTP', 'Phase', 'Speed', 'Spider', 'SpinBot', 'Swim', 'TargetStrafe', 'Timer',
+    'AimAssist', 'AutoClicker', 'Reach', 'SilentAim', 'TriggerBot',
+    'Atmosphere', 'Breadcrumbs', 'Cape', 'ChinaHat', 'Clock', 'Disguise', 'FOV', 'FPS',
+    'Keystrokes', 'Memory', 'Ping', 'SongBeats', 'Speedmeter', 'TimeChanger',
+    'MurderMystery',
+    'Arrows', 'Chams', 'ESP', 'Fullbright', 'GamingChair', 'Health', 'NameTags',
+    'PlayerModel', 'Radar', 'Search', 'SessionInfo', 'Tracers', 'Waypoints',
+    'AnimationPlayer', 'AntiRagdoll', 'AutoRejoin', 'Blink', 'ChatSpammer', 'Disabler',
+    'Panic', 'Rejoin', 'ServerHop', 'StaffDetector', 'StateSpoofer',
+    'Anti-AFK', 'FastProxPrompt', 'Freecam', 'Gravity', 'Parkour', 'SafeWalk', 'Wallhop', 'Xray'
+} do
+    vape:Remove(v);
+end;
+
 vape.run(function()
     local TestPrint: table = { ["Enabled"] = false };
 
